77f61b6270002d66fd182c41b8fbcc7d2227ac9b
I Gede Surya Rahayuda <surya.rahayuda@gmail.com>
2023-04-19 23:14:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/volleyselect/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.volleyselect;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    public static final String URL = "https://websensordata.000webhostapp.com/android/select.php";
+    //public static final String URL = "http://192.168.1.5:8080/kampus/android/select.php";
+
+    ListView list;
+    SwipeRefreshLayout swipe;
+    List<Data> itemList = new ArrayList<Data>();
+    MhsAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        swipe = (SwipeRefreshLayout) findViewById(R.id.swipe);
+        list = (ListView) findViewById(R.id.list);
+
+        adapter = new MhsAdapter(MainActivity.this, itemList);
+        list.setAdapter(adapter);
+
+        swipe.setOnRefreshListener(this);
+
+        swipe.post(new Runnable() {
+                       @Override
+                       public void run() {
+                           swipe.setRefreshing(true);
+                           itemList.clear();
+                           adapter.notifyDataSetChanged();
+                           callVolley();
+                       }
+                   }
+        );
+    }
+
+    @Override
+    public void onRefresh() {
+        //  itemList.clear();
+        //  adapter.notifyDataSetChanged();
+        callVolley();
+    }
+
+    private void callVolley() {
+        itemList.clear();
+        adapter.notifyDataSetChanged();
+        swipe.setRefreshing(true);
+
+        // membuat request JSON
+        JsonArrayRequest jArr = new JsonArrayRequest(URL, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                // Parsing json
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject obj = response.getJSONObject(i);
+
+                        Data item = new Data();
+
+                        item.setId(obj.getString("id"));
+                        item.setNim(obj.getString("nim"));
+                        item.setNama(obj.getString("nama"));
+                        item.setAlamat(obj.getString("alamat"));
+
+                        // menambah item ke array
+                        itemList.add(item);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                // notifikasi adanya perubahan data pada adapter
+                adapter.notifyDataSetChanged();
+
+                swipe.setRefreshing(false);
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "gagal koneksi ke server, cek setingan koneksi anda", Toast.LENGTH_LONG).show();
+                swipe.setRefreshing(false);
+            }
+        });
+
+        // menambah request ke request queue
+        RequestQueue mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        mRequestQueue.add(jArr);
+
+    }
+
+}
